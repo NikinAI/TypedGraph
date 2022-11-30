4d3e755c6d614e159bc869b94f6ede0774f79d35
Leo Benkel <4960573+leobenkel@users.noreply.github.com>
2022-11-30 16:30:24 +0100

    Add `>>>` method to link vertex together using Path and Edges (#7)

--- src/main/scala/ai/nikin/typedgraph/core/Edge.scala
+++ src/main/scala/ai/nikin/typedgraph/core/Edge.scala
@@ -1,6 +1,8 @@
 package ai.nikin.typedgraph.core
 
-class Edge(val from: Vertex, val to: Vertex) {}
+class Edge(val from: Vertex, val to: Vertex) {
+  def >>>(next: Vertex): Path = Path(this, to >>> next)
+}
 
 object Edge {
   def apply(from: Vertex, to: Vertex): Edge = new Edge(from, to)
---
+++ src/test/scala/ai/nikin/typedgraph/core/PathTest.scala
@@ -0,0 +1,27 @@
+package ai.nikin.typedgraph.core
+
+import ai.nikin.typedgraph.core.testUtils.Test
+
+class PathTest extends Test("Path") {
+  test("create/retrieve") {
+    val v1 = Vertex("1")
+    val v2 = Vertex("2")
+    val v3 = Vertex("3")
+    val e1 = Edge(v1, v2)
+    val e2 = Edge(v2, v3)
+    val p  = Path(e1, e2)
+
+    assertEquals(p.from, v1)
+    assertEquals(p.to, v3)
+    assertEquals(p.edges, List(e1, e2))
+    assertEquals(p.vertices, List(v1, v2, v3))
+
+  }
+
+  test("connect") {
+    val p = Vertex("a") >>> Vertex("b") >>> Vertex("c") >>> Vertex("d")
+    assertEquals(p.from.label, "a")
+    assertEquals(p.to.label, "d")
+    assertEquals(p.vertices.map(_.label), List("a", "b", "c", "d"))
+  }
+}
