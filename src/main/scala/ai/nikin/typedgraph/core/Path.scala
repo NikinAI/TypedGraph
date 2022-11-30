359491a64f71936ab9862038e76b80567c8ed146
Leo Benkel <leo.benkel@gmail.com>
2022-11-30 15:18:51 +0100

    Use list instead of Set

--- src/main/scala/ai/nikin/typedgraph/core/Path.scala
+++ src/main/scala/ai/nikin/typedgraph/core/Path.scala
@@ -1,12 +1,13 @@
 package ai.nikin.typedgraph.core
 
-class Path(edges: Set[Edge]) {
+class Path(edges: List[Edge]) {
   lazy val from: Vertex = edges.head.from
   lazy val to:   Vertex = edges.last.to
 
-  def >>>(next: Vertex): Path = new Path(edges + (to >>> next))
+  def >>>(next: Vertex): Path = Path(edges, to >>> next)
 }
 
 object Path {
-  def apply(edges: Edge*): Path = new Path(edges.toSet)
+  def apply(edges: Edge*): Path = new Path(edges.toList)
+  def apply(edges: List[Edge], moreEdges: Edge*): Path = new Path(edges ++ moreEdges)
 }
