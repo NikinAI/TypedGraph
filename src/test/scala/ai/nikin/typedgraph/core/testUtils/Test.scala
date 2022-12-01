e0413c5dad26a597dd775822f599020ae30298ab
Leo Benkel <leo.benkel@gmail.com>
2022-12-01 11:13:39 +0100

    Add compile flags

--- src/test/scala/ai/nikin/typedgraph/core/testUtils/Test.scala
+++ src/test/scala/ai/nikin/typedgraph/core/testUtils/Test.scala
@@ -1,7 +1,5 @@
 package ai.nikin.typedgraph.core.testUtils
 
-import munit.Location
-
 abstract class Test(name: String) extends munit.FunSuite {
 
   // https://scalameta.org/munit/docs/tests.html#customize-test-name-based-on-a-dynamic-condition
