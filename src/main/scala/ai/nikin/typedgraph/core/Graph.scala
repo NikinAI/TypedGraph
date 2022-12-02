db2735d50d0b4852ecdec10bbc368a14ffa25784
Leo Benkel <leo.benkel@gmail.com>
2022-12-02 14:35:52 +0100

    Implement and test foldLeft to traverse the Graph

--- src/main/scala/ai/nikin/typedgraph/core/Graph.scala
+++ src/main/scala/ai/nikin/typedgraph/core/Graph.scala
@@ -2,68 +2,111 @@ package ai.nikin.typedgraph.core
 
 import ai.nikin.typedgraph.core.Utils._
 
+import scala.annotation.unused
 import scala.collection.immutable.ListSet
 
-class Graph private (val edges: ListSet[AnyEdge]) {
+class Graph private (val edges: ListSet[AnyEdge]) { self =>
+  import Graph._
 
-  lazy private[core] val leftBoundary: Set[AnyVertex] =
-    flattenedEdges.map(_.from) -- flattenedEdges.map(_.to)
-  lazy private[core] val rightBoundary: Set[AnyVertex] =
-    flattenedEdges.map(_.to) -- flattenedEdges.map(_.from)
+  // TODO: Make sure to never commit other code than `()`
+  protected def debug(message: String): Unit = ()
 
-  lazy private[core] val leftEdgeBoundary: Set[AnyEdge] =
-    leftBoundary.flatMap(findEdgesWithFrom(flattenedEdges))
-  lazy private[core] val rightEdgeBoundary: Set[AnyEdge] =
-    rightBoundary.flatMap(findEdgesWithTo(flattenedEdges))
+  implicit private class MapExtra[T](m: Map[Int, ListSet[T]]) {
+    def addAt(k: Int)(extra: ListSet[T]): Map[Int, ListSet[T]] =
+      if(extra.isEmpty) m
+      else m.updatedWith(k) {
+        case None    => Some(extra)
+        case Some(s) => Some(s ++ extra)
+      }
 
-  lazy private[core] val vertexPerDepth: Map[Int, Set[AnyVertex]] =
+    def addOneAt(k: Int)(extra: T): Map[Int, ListSet[T]] = m.addAt(k)(ListSet(extra))
+
+    def removeOneAt(k: Int)(remove: T): Map[Int, ListSet[T]] =
+      m.updatedWith(k) {
+        case None    => None
+        case Some(s) => s - remove match {
+            case r if r.isEmpty => None
+            case r              => Some(r)
+          }
+      }
+
+    def cleanPreviousKeys(): Map[Int, ListSet[T]] =
+      m.keys
+        .filterNot(_ == 0)
+        .toList
+        .sortBy(-_)
+        .foldLeft(m) {
+          case (acc, k) => m(k).foldLeft(acc) {
+              case (acc, v) => (1 to k).foldLeft(acc) { case (acc, d) => acc.removeOneAt(k - d)(v) }
+            }
+        }
+
+    def deepFoldLeft[B](init: B)(f: (B, T) => B): B =
+      m.toList
+        .sortBy(_._1)
+        .foldLeft(init) {
+          case (acc, (k, v)) => v
+              .toList
+              .sortBy(_.toString)
+              .foldLeft(acc) {
+                case (acc, v) =>
+                  debug(s"$k -> $v")
+                  f(acc, v)
+              }
+        }
+  }
+
+  lazy private val leftBoundary:              ListSet[AnyVertex] = edges.map(_.from) -- edges.map(_.to)
+  lazy private val rightBoundary:             ListSet[AnyVertex] = edges.map(_.to) -- edges.map(_.from)
+  lazy private val leftEdgeBoundary:          ListSet[AnyEdge]   = leftBoundary.flatMap(findEdgesFrom(edges))
+  @unused lazy private val rightEdgeBoundary: ListSet[AnyEdge]   =
+    rightBoundary.flatMap(findEdgesTo(edges))
+
+  lazy private val vertexPerDepth: Map[Int, ListSet[AnyVertex]] =
     edgePerDepth
-      .foldLeft(Map.empty[Int, Set[AnyVertex]]) {
+      .foldLeft(Map.empty[Int, ListSet[AnyVertex]]) {
         case (acc, (k, edges)) => acc.addAt(k)(edges.map(_.from)).addAt(k + 1)(edges.map(_.to))
       }
       .cleanPreviousKeys()
 
-  lazy private[core] val edgePerDepth: Map[Int, Set[AnyEdge]] = {
+  lazy private val edgePerDepth: Map[Int, ListSet[AnyEdge]] = {
     def loop(
-      depth: Int,
-      acc: Map[Int, Set[AnyEdge]],
-      edgesLeft: Set[AnyEdge],
-      node: AnyVertex,
-    ): Map[Int, Set[AnyEdge]] =
-      findEdgesWithFrom(edgesLeft)(node).foldLeft(acc) {
-        case (acc, e@Edge(_, to)) => loop(
-          depth = depth + 1,
-          acc = acc.addOneAt(depth)(e),
-          edgesLeft = edgesLeft - e,
-          node = to,
-        )
+        depth:     Int,
+        acc:       Map[Int, ListSet[AnyEdge]],
+        edgesLeft: ListSet[AnyEdge],
+        node:      AnyVertex,
+    ): Map[Int, ListSet[AnyEdge]] =
+      findEdgesFrom(edgesLeft)(node).foldLeft(acc) {
+        case (acc, e @ Edge(_, to)) => loop(
+            depth = depth + 1,
+            acc = acc.addOneAt(depth)(e),
+            edgesLeft = edgesLeft - e,
+            node = to,
+          )
       }
 
     leftEdgeBoundary
-      .foldLeft(Map.empty[Int, Set[AnyEdge]]) {
+      .foldLeft(Map.empty[Int, ListSet[AnyEdge]]) {
         case (acc, e) => loop(
-          depth = 1,
-          acc = acc.addOneAt(0)(e),
-          edgesLeft = self.flattenedEdges,
-          node = e.to,
-        )
+            depth = 1,
+            acc = acc.addOneAt(0)(e),
+            edgesLeft = self.edges,
+            node = e.to,
+          )
       }
       .cleanPreviousKeys()
   }
 
   def foldVertexLeft[A](init: A)(f: (A, AnyVertex) => A): A = vertexPerDepth.deepFoldLeft(init)(f)
-
-  def foldEdgeLeft[A](init: A)(f: (A, AnyEdge) => A): A = edgePerDepth.deepFoldLeft(init)(f)
+  def foldEdgeLeft[A](init:   A)(f: (A, AnyEdge) => A):   A = edgePerDepth.deepFoldLeft(init)(f)
 }
 
 object Graph {
   def apply(edges: AnyEdge*): Graph = new Graph(edges.toListSet.flatMap[AnyEdge](_.flatten))
 
-  private[core] def findEdgesWithFrom(
-      edges: Set[AnyEdge]
-  )(from:    AnyVertex): Set[AnyEdge] = edges.flatMap(_ filterWithFrom from)
+  private def findEdgesFrom(edges: ListSet[AnyEdge])(from: AnyVertex): ListSet[AnyEdge] =
+    edges.flatMap(_ filterFrom from)
 
-  private[core] def findEdgesWithTo(
-      edges: Set[AnyEdge]
-  )(to:      AnyVertex): Set[AnyEdge] = edges.flatMap(_ filterWithTo to)
+  private def findEdgesTo(edges: ListSet[AnyEdge])(to: AnyVertex): ListSet[AnyEdge] =
+    edges.flatMap(_ filterTo to)
 }
--- src/test/scala/ai/nikin/typedgraph/core/GraphTest.scala
+++ src/test/scala/ai/nikin/typedgraph/core/GraphTest.scala
@@ -16,4 +16,71 @@ class GraphTest extends Test("Graph") {
 
     assertEquals(g.edges, ListSet(e1, e2))
   }
+
+  test("foldLeft") {
+    val v1 = Vertex("1")
+    val v2 = Vertex("2")
+    val v3 = Vertex("3")
+    val e1 = v1 >?> v2
+    val e2 = v2 >?> v3
+
+    val g = e1 |+| e2
+
+    val counter =
+      g.foldVertexLeft(0) {
+        case (counter, v) if v == v1 =>
+          assert(counter == 0, counter)
+          counter + 1
+        case (counter, v) if v == v2 =>
+          assert(counter == 1, counter)
+          counter + 1
+        case (counter, v) if v == v3 =>
+          assert(counter == 2, counter)
+          counter + 1
+        case (counter, v)            => fail(s"Unexpected to find ($counter, $v) here.")
+      }
+    assert(counter == 3, counter)
+  }
+
+  test("foldLeft with 2 paths") {
+    val v1  = Vertex("v1")
+    val v2a = Vertex("v2a")
+    val v2b = Vertex("v2b")
+    val v3  = Vertex("v3")
+
+    val g: Graph = (v1 >?> v2a >?> v3) |+| (v1 >?> v2b >?> v3)
+
+    val counter =
+      g.foldVertexLeft(0) {
+        case (counter, v) if v == v1  =>
+          assert(counter == 0, counter)
+          counter + 1
+        case (counter, v) if v == v2a =>
+          assert(counter == 1, counter)
+          counter + 1
+        case (counter, v) if v == v2b =>
+          assert(counter == 2, counter)
+          counter + 1
+        case (counter, v) if v == v3  =>
+          assert(counter == 3, counter)
+          counter + 1
+        case v                        => fail(s"Unexpected to find $v here.")
+      }
+    assert(counter == 4, counter)
+  }
+
+  test("cycle with start") {
+    val a        = Vertex("a")
+    val b        = Vertex("b")
+    val c        = Vertex("c")
+    val entrance = Vertex("entrance")
+
+    val g: Graph = (entrance >?> a >?> b >?> c >?> a).asGraph
+
+    val edgesCount = g.foldEdgeLeft(0)((c, _) => c + 1)
+    assert(edgesCount == 4, edgesCount)
+
+    val vertexCount = g.foldVertexLeft(0)((c, _) => c + 1)
+    assert(vertexCount == 4, vertexCount)
+  }
 }
